fc8854dce7515187d33ff4a9f61ecdb266f9bcb0
jacobtarter <jtarter01@aurora.edu>
2017-01-22 23:17:49 -0600

    new routes, added to model
    SmartGoal model now has a field for “complete” (boolean), which
    defaults as true when created.
    
    I also created a route to get goals by user_id, which will be the main
    get route we actually use in the app. We don’t want users to be able to
    get goals for all users, just themselves.
    
    I also added a route for setting a goal as complete, by providing the
    id of the goal
    
    Started a google doc with current api routes:
    https://docs.google.com/document/d/16MAEpKa1scsLxBQTfCrCMynH6lxVX9G5p9db
    C5TQ_eE/edit?usp=sharing

--- models/smartgoal.js
+++ models/smartgoal.js
@@ -10,17 +10,32 @@ var SmartGoalSchema = new Schema({
   title: {type: String, required: true},
   description: String, //Do we want description to be required?
   user_id: {type: Number, required: true}, //Number or string?
-  user_token: {type: String, required: true},
+  //user_token: {type: String, required: true}  <-- By removing from schema, token should not be stored in object
   difficulty: {type: String, enum: diff, required: true},
   goal_type: {type: String, enum: types, required: true},
   due_date: Date,
-  repeat: {type: String, enum: rep}
+  repeat: {type: String, enum: rep},
+  complete: Boolean
 
 });
 
 
 SmartGoalSchema.pre('save', function(next){
   //In this pre save middleware we would also check for a valid user_id & token
+  if (!this.user_token)
+  {
+    return next(new Error("ERROR: Must provide a user_token"));
+  }
+  if (!this.user_id)
+  {
+    return next(new Error("ERROR: Must provide a user_id"));
+  }
+  /*
+    function to check the validity of user_token
+    if user_token is not valid, return next(new Error("invalid token"))
+
+
+  */
   if (this.goal_type == "SINGLE" && !this.due_date)
   {
     return next(new Error("ERROR: Must set due_date when goal_type is SINGLE"));
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,6 +17,48 @@ router.route('/goal')
 		});
 });
 
+router.route('/goal/byuser')
+	.post(function(req, res) {
+		var query = {};
+		query.user_id = req.body.user_id;
+		SmartGoal.find(query, function (err, goals) {
+			if (err)
+			{
+				res.send(err);
+			}
+			res.json(goals);
+		});
+	});
+
+router.route('/goal/complete')
+	.post(function(req, res) {
+		goal = SmartGoal.findById(req.body.goal_id, function(err, goal) {
+			if (err)
+			{
+				res.send(err);
+			}
+			goal.complete = true;
+			goal.user_id = req.body.user_id;
+			goal.user_token = req.body.user_token;
+
+			/*
+				Code for calculating points earned
+				Send points to user object, add points earned to JSON response
+
+			*/
+
+			goal.save(function(err) {
+				if (err)
+				{
+					res.send(err.message);
+				}
+				else {
+					res.json({message: "Goal set as complete."});
+				}
+			});
+		});
+	});
+
 router.route('/goal')
 
 	.post(function(req, res) {
@@ -35,6 +77,7 @@ router.route('/goal')
 			goal.due_date = req.body.due_date;
 		}
 		goal.repeat = req.body.repeat;
+		goal.complete = false;
 
 
 			goal.save(function(err) {
