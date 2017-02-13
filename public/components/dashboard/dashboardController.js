bac3a40ac54242013517ad4acc7db29beb7db43e
Zach <zachary.gordon@gmail.com>
2017-02-13 17:16:28 -0600

    a few small fixes after the merge

--- public/components/dashboard/dashboardController.js
+++ public/components/dashboard/dashboardController.js
@@ -1,21 +1,16 @@
-angular.module('app').controller('DashboardController', ['$state', 'authService', 'profileService', 'jwtHelper', '$state', 'authManager', function($state, authService, profileService, jwtHelper, $state, authManager) {
-  if (localStorage.getItem('id_token') && !jwtHelper.isTokenExpired(localStorage.getItem('id_token'))) {
-      console.log("auth good");
-  } else {
-      authManager.unauthenticate();
-      $state.transitionTo('home');
-    }
-
-  var dash = this;
-
-
-
-
+angular.module('app').controller('DashboardController', ['$state', 'authService', 'jwtHelper', '$state', 'authManager', 'userService', function($state, authService, jwtHelper, $state, authManager, userService) {
+    var dash = this;
     init();
-
     function init() {
         console.log("dash init ran");
-        userService.sync();
+        if (localStorage.getItem('id_token') && !jwtHelper.isTokenExpired(localStorage.getItem('id_token'))) {
+            userService.sync();
+            console.log("auth good");
+        } else {
+            userService.logout();
+            authManager.unauthenticate();
+            $state.transitionTo('home');
+        }
     }
-    var dash = this;
+
 }]);
