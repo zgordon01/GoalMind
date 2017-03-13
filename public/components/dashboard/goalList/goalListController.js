63af666c485a73bb51e66d94c65dc651e68ef1e0
jazmina47 <jazminm0477@gmail.com>
2017-03-13 11:56:35 -0500

    added Bootstrap-dialog modal for "complete" button
    Added bootstrap3-dialog to front end and edited setAsComplete function
    to give a styled modal instead of a standard confirm alert for the goal
    List view (active goals) when clicking on the “Complete!” button.

--- public/components/dashboard/goalList/goalListController.js
+++ public/components/dashboard/goalList/goalListController.js
@@ -51,11 +51,14 @@ angular.module('app').controller('GoalListController', ['$scope', 'goalService',
   $scope.goToNewGoal = function() {
     $state.transitionTo("dashboard.newGoal");
   }
+
   $scope.setAsComplete = function(goalId, goalTitle) {
-    confirmComplete = confirm("Mark the goal '" + goalTitle + "' as complete?");
-    if(confirmComplete)
-    {
-      goalService.setAsComplete(goalId, function(response) {
+
+    BootstrapDialog.confirm("Mark the goal '" + goalTitle + "' as complete?", function(confirmComplete){
+      //console.log("from setAsComplete function (glc): " + confirmComplete);
+      if(confirmComplete) {
+ 
+        goalService.setAsComplete(goalId, function(response) {
         if(response.message == "Goal Complete!")
         {
           $scope.refreshGoals();
@@ -63,6 +66,13 @@ angular.module('app').controller('GoalListController', ['$scope', 'goalService',
           //jquery scroll to top with animation, doesnt seem to work: $('html, body').animate({ scrollTop: 0 }, 'fast');
         }
       });
-    }
+
+      }
+      else {
+        //close the dialog window
+        close();
+      }
+    }); 
   }
-}]);
+
+}]); //end of controller
