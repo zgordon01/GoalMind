04b81bdd83f3cf891633adcb766dcee992f0c750
jacobtarter <jtarter01@aurora.edu>
2017-04-04 07:15:56 -0500

    Added auto-collapse to side nav
    I added a class to all side nav links which auto collapses the nav. When you click a link, the nav goes away and the new page opens

--- public/components/js/navbar.js
+++ public/components/js/navbar.js
@@ -2,23 +2,23 @@ $(function () {
     $('.navbar-toggle').click(function () {
         $('.navbar-nav').toggleClass('slide-in');
         $('.side-body').toggleClass('body-slide-in');
-        $('#search').removeClass('in').addClass('collapse').slideUp(200);
-
+        //$('#search').removeClass('in').addClass('collapse').slideUp(200);
         /// uncomment code for absolute positioning tweek see top comment in css
         //$('.absolute-wrapper').toggleClass('slide-in');
 
     });
 
-   // Remove menu for searching
-   $('#search-trigger').click(function () {
-        $('.navbar-nav').removeClass('slide-in');
-        $('.side-body').removeClass('body-slide-in');
-
+    //This will hide the nav if you click on any of the sidenav links (add class = toggle for this effect)
+    $('.toggle').click(function () {
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+        //$('#search').removeClass('in').addClass('collapse').slideUp(200);
         /// uncomment code for absolute positioning tweek see top comment in css
-        //$('.absolute-wrapper').removeClass('slide-in');
+        //$('.absolute-wrapper').toggleClass('slide-in');
 
     });
 
+    //This will hide nav if you click into the main document
     //fix for side nav not hiding
     $(document).click(function(event) {
         if($(event.target).closest('#main-content').length){
